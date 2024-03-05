070b1e0b723c42f2e2b5ab684f67b36bae7c1d5a
swathi <swathiskumar96@gmail.com>
2024-03-05 11:21:18 +0530

    counter app

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Counter from './components/Counter'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-center mt-4'>Counter App</h1>
+    <div style={{height:'70vh'}} className='d-flex justify-content-center alogn utems-center w-100 mt-4'>
+    <Counter/>
+
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import counterStore from './counterStore.js'
+import { Provider } from 'react-redux'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={counterStore}> <App /> </Provider>
+  </React.StrictMode>,
+)
